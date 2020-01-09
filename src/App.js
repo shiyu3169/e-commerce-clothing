34539f752c1a71865fba33c5ac6f5ab3cea6cb0d
Shiyu Wang <shiyu3169@gmail.com>
2020-01-09 09:57:19 -0500

    1. creating header component and adding it to all pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import Home from "./pages/Home/Home";
-import "./App.css";
 import { Route, Switch } from "react-router-dom";
+import "./App.css";
+// Components
+import Home from "./pages/Home/Home";
 import Shop from "./pages/Shop/Shop";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/shop" component={Shop} />
---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Header.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Link to="/" className="logo-container">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link to="/shop" className="option">
+          SHOP
+        </Link>
+        <Link to="/contact" className="option">
+          CONTACT
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
