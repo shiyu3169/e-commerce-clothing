f2c191bce32e817be60aa129b78fbb3a57c5a3e2
Shiyu Wang <shiyu3169@gmail.com>
2020-01-13 10:42:27 -0500

    sign up with email and password, link it with firebase

--- src/App.js
+++ src/App.js
@@ -20,8 +20,22 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state);
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
--- src/pages/Sign/Sign.js
+++ src/pages/Sign/Sign.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Sign.scss";
 import SignIn from "../../components/SignIn/SignIn";
+import SignUp from "../../components/SignUp/SignUp";
 
 const Sign = () => {
   return (
     <div className="sign">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
