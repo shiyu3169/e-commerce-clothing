c68ef041e55bc0ef3830422bb91dd15817072615
Shiyu Wang <shiyu3169@gmail.com>
2020-01-14 13:12:04 -0500

    adding items into cart dropdown

--- src/components/CartDropdown/CartDropdown.js
+++ src/components/CartDropdown/CartDropdown.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./CartDropdown.scss";
 import Button from "../Button/Button";
-const CartDropdown = () => {
+import CartItem from "../CarItem/CartItem";
+const CartDropdown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
 };
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/CollectionItem/CollectionItem.js
+++ src/components/CollectionItem/CollectionItem.js
@@ -11,7 +11,7 @@ const CollectionItem = ({ item, addItem }) => {
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className="collection-footer">
         <span className="name">{name}</span>
-        <span className="price">{price}</span>
+        <span className="price">${price}</span>
       </div>
       <Button inverted onClick={() => addItem(item)}>
         {" "}
